ecc82d9e414a29f2847ccdfa1f5dbd3bbb562488
Shakeria Smith <shakerias2@gmail.com>
2018-07-10 16:41:52 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,3 +18,16 @@ def valid_move?(board,position)
      false
   end
 end
+
+def position_taken?(board,position)
+  if board[position] == " "
+    false
+    elsif board[position] == ""
+    false
+    elsif  board[position] == nil
+    false
+    else   board[position] == "X" || "O"
+    true
+
+  end
+end
