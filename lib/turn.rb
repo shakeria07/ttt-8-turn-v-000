741611b12e0b2b11b48f8fef54da021fe87da9f6
Shakeria Smith <shakerias2@gmail.com>
2018-07-10 17:12:40 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,22 +1,43 @@
-def display_board(board)
-
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+def turn(board)
+ puts "Please enter 1-9:"
+ input = gets.strip
+input = input_to_index(input)
+ valid = valid_move?(board, input)
+ if valid == true
+     move(board, input, "X")
+     display_board(board)
+ else
+   puts "please enter 1-9:"
+  input = gets.strip
+ end
 end
 
-def input_to_index(user_input)
-  user_input.to_i-1
+ def display_board(board)
+   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+   puts "-----------"
+   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts "-----------"
+   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
-def valid_move?(board,position)
-  position.to_i.between?(0,8) and not position_taken?(board, position.to_i - 1)
+def input_to_index(input)
+  input = input.to_i-1
 end
-
-def position_taken?(board,position)
-  return false if [" ", "", nil].include?(board[position])
-  return true if ["X", "O"].include?(board[position])
-  raise "#{board[position]} is not a valid move"
+def move(board, input, value="X")
+   board[input] = value
+  end
+  def valid_move?(board, index)
+     if index == nil || index < 0 || index > 8
+       false
+     elsif position_taken?(board, index)
+     false
+    else
+      true
+    end
+end
+def position_taken?(board, index)
+   if board[index] == "X" || board[index] ==  "O"
+     true
+   elsif board[index] == " " || board[index] == "" || board[index] == nil
+     false
+  end
 end
